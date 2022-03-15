2980c1b44f65e8be81b7f34cce884c748e27fd3e
AnatolieQACurs <101135285+AnatolieQACurs@users.noreply.github.com>
2022-03-15 19:09:38 +0200

    SarcinaPracticaNR.7

--- src/com/company/topic1/TestAngajat.java
+++ src/com/company/topic1/TestAngajat.java
@@ -2,7 +2,7 @@ package com.company.topic1;
 
 public class TestAngajat {
     public static void main(String[] args) {
-        Angajat ang =new Angajat("Anatolie" , 23 ,  15000.00 , "Masculin");
+        Angajat ang = new Angajat("Anatolie" , 23 ,  15000.00 , "Masculin");
         ang.afiseazaInfo();
     }
 }
