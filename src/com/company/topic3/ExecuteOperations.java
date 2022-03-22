740270b4d1abb5e3a5cd489d88e8a8d2ca73f8ff
AnatolieQACurs <101135285+AnatolieQACurs@users.noreply.github.com>
2022-03-22 22:17:14 +0200

    Sarcina topic 3

--- src/com/company/topic3/ExecuteOperations.java
+++ src/com/company/topic3/ExecuteOperations.java
@@ -1,12 +1,7 @@
 package com.company.topic3;
 
-
 public class ExecuteOperations {
     public static void main(String[] args) {
         Operations.printLogicOperations();
-       //Operations.printAritmeticOperations();
-       //Operations.printRelationarOperations();
-        //Operations.useForAndWhile();
-
     }
 }
